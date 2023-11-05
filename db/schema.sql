66eb6cd61614733ac7d06e1c89a6346ff0da92a4
Mysmelf <matthewr9937@gmail.com>
2023-11-05 12:56:52 -0800

    Used foreign keys to link table data, still needs to be tested, need to get the server / routes / seeds up and running first

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,27 @@ CREATE DATABASE employee_db;
 USE employee_db;
     
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT, -- link department id to department table id $
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT, -- link role id to role table id #
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL -- link department id to department table id $
-    PRIMARY KEY (id)
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT, -- link manager to employee id @
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL AUTO_INCREMENT, -- link role id to role table id #
-    manager_id INT NOT NULL AUTO_INCREMENT, -- link manager to employee id @
-    PRIMARY KEY (id)
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
+    FOREIGN KEY (manager_id) REFERENCES (id)
 );
