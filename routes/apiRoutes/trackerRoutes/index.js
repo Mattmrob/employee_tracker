baccdbbc1db195f6edfdc46e44888910cd191071
Mysmelf <matthewr9937@gmail.com>
2023-11-05 13:08:41 -0800

    Created initial routes and server file contents - need to establish controllers to build into express routes on api file

---
+++ routes/apiRoutes/trackerRoutes/index.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+// const { getTodos, createTodo, updateTodoById, deleteTodoById, getTodoById } = require("../../../controllers/todosController")
+// example from previous project, need to destructure controllers from controller folder
+
+// router.get('/', getTodos);
+// router.post('/', createTodo);
+
+// router.route('/')
+//     .get(getTodos)
+//     .post(createTodo);
+
+// // for doing anything to a specific row in our table
+// router.route('/:id')
+//     .get(getTodoById)
+//     .put(updateTodoById)
+//     .delete(deleteTodoById);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// middleware for req.body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
